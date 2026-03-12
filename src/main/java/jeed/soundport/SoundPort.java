b988b2252cb8d9cbbac4567e2d7a298dfed751c8
jeedrb <jar3d.xd@gmail.com>
2026-03-12 14:43:08 -0400

    now playable in a standard environment!

--- src/main/java/jeed/soundport/SoundPort.java
+++ src/main/java/jeed/soundport/SoundPort.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class SoundPort
 {
     public static final String MODID = "soundport";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.5";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
