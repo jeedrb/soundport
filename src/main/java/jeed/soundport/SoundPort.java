b75ea47aeb5f154ec8f4196a26d3c8880d6c57e0
jeedrb <jar3d.xd@gmail.com>
2026-03-12 12:18:42 -0400

    sounds vary in pitch

--- src/main/java/jeed/soundport/SoundPort.java
+++ src/main/java/jeed/soundport/SoundPort.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class SoundPort
 {
     public static final String MODID = "soundport";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.2";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
--- src/main/java/jeed/soundport/SoundPortEvents.java
+++ src/main/java/jeed/soundport/SoundPortEvents.java
@@ -6,30 +6,36 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class SoundPortEvents {
+
+    // witch hurt sound
     @SubscribeEvent
     public void onWitchHurt(LivingHurtEvent event) {
-        if (event.entityLiving != null && !event.entityLiving.worldObj.isRemote) {
-            if (event.entityLiving instanceof EntityWitch) {
-                if (event.entityLiving.getHealth() - event.ammount > 0) {
+        if (event.entityLiving != null && event.entityLiving.worldObj != null && !event.entityLiving.worldObj.isRemote) { // check if it exists
+            if (event.entityLiving instanceof EntityWitch) { // is it a witch
+                if (event.entityLiving.getHealth() - event.ammount > 0) { // waiter one hurt sound please!
                     event.entityLiving.worldObj.playSoundAtEntity(
                             event.entityLiving,
                             "soundport:witch.hurt",
                             1.0F,
-                            1.0F
+                            // random pitch 0.8-1.2
+                            0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F)
                     );
                 }
             }
         }
     }
+
+    // witch death sound
     @SubscribeEvent
     public void onWitchDeath(LivingDeathEvent event) {
-        if (event.entityLiving instanceof EntityWitch) {
-            if (event.entityLiving != null && event.entityLiving.worldObj != null) {
-                event.entityLiving.worldObj.playSoundAtEntity(
+        if (event.entityLiving != null && event.entityLiving.worldObj != null && !event.entityLiving.worldObj.isRemote) { // check if it exists
+            if (event.entityLiving instanceof EntityWitch) { // is it a witch
+                event.entityLiving.worldObj.playSoundAtEntity( // waiter one death sound please!
                         event.entityLiving,
                         "soundport:witch.death",
                         1.0F,
-                        1.0F
+                        // random pitch 0.8-1.2
+                        0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F)
                 );
             }
         }
