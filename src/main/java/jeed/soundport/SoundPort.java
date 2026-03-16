05c90a555480c857fa4ae91986977cc2288fab23
jeedrb <jar3d.xd@gmail.com>
2026-03-16 11:18:31 -0400

    cleaned code, added banners and skulls

--- src/main/java/jeed/soundport/SoundPort.java
+++ src/main/java/jeed/soundport/SoundPort.java
@@ -1,26 +1,19 @@
 package jeed.soundport;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.FolderResourcePack;
-import net.minecraft.client.resources.IResourcePack;
-import net.minecraft.client.resources.ResourcePackRepository;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
-
 import java.io.File;
-import java.util.List;
 
 @Mod(modid = SoundPort.MODID, version = SoundPort.VERSION)
 public class SoundPort
 {
     public static final String MODID = "soundport";
-    public static final String VERSION = "0.7.4";
+    public static final String VERSION = "0.8";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
--- src/main/java/jeed/soundport/SoundPortEvents.java
+++ src/main/java/jeed/soundport/SoundPortEvents.java
@@ -1,9 +1,6 @@
 package jeed.soundport;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockBanner;
-import net.minecraft.block.BlockBed;
-import net.minecraft.block.BlockDoor;
+import net.minecraft.block.*;
 import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.entity.item.EntityPainting;
@@ -24,17 +21,21 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class SoundPortEvents {
 
+    private static final String soundArray[] = {
+        "", // 0 BLANK FOR OTHER BLOCKS AND ITEMS
+        "soundport:wood.place", // 1 WOOD
+        "soundport:wool.place", // 2 CLOTH/WOOL
+        "soundport:green.place", // 3 GRASS/CROPS/GREEN
+        "soundport:stone.place", // 4 STONE/IRON
+        "soundport:witch.throw" // 5 POTION THROW
+    };
+
     // witch hurt sound
     @SubscribeEvent
     public void onWitchHurt(LivingHurtEvent event) {
         if (event.entityLiving != null && event.entityLiving.worldObj != null && !event.entityLiving.worldObj.isRemote && event.entityLiving instanceof EntityWitch) { // check if it exists, exists in the world, exists on the server, and is a witch
             if (event.entityLiving.getHealth() - event.ammount > 0) { // checks if it's about to die so it can do witch.death instead
-                event.entityLiving.worldObj.playSoundAtEntity( // waiter one hurt sound please!
-                    event.entityLiving,
-                    "soundport:witch.hurt",
-                    1.0F,
-                    0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F) // random pitch 0.8-1.2
-                );
+                event.entityLiving.worldObj.playSoundAtEntity(event.entityLiving,"soundport:witch.hurt",1.0F, 0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F));
             }
         }
     }
@@ -43,12 +44,7 @@ public class SoundPortEvents {
     @SubscribeEvent
     public void onWitchDeath(LivingDeathEvent event) {
         if (event.entityLiving != null && event.entityLiving.worldObj != null && !event.entityLiving.worldObj.isRemote && event.entityLiving instanceof EntityWitch) { // check if it exists, exists in the world, exists on the server, and is a witch
-            event.entityLiving.worldObj.playSoundAtEntity( // waiter one death sound please!
-                event.entityLiving,
-                "soundport:witch.death",
-                1.0F,
-                0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F) // random pitch 0.8-1.2
-            );
+            event.entityLiving.worldObj.playSoundAtEntity(event.entityLiving,"soundport:witch.death",1.0F, 0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F));
         }
     }
 
@@ -60,43 +56,11 @@ public class SoundPortEvents {
             float pitch = 0.8F + (event.entityLiving.getRNG().nextFloat() * 0.4F); // random pitch 0.8-1.2
 
             // 1% chance every tick to do an evil as freak witch idle noise, yikes!
-            if (witch.getRNG().nextInt(100) == 67) { // this could be anything from 0-99 but 67 is epic sauce so that is my magic number
-                witch.worldObj.playSoundAtEntity( // waiter one idle sound please!
-                    witch,
-                    "soundport:witch.idle",
-                    1.0F,
-                    pitch
-                );
-            }
+            if (witch.getRNG().nextInt(100) == 67) witch.worldObj.playSoundAtEntity(witch,"soundport:witch.idle",1.0F, pitch);
 
+            // sound when drinking a potion to heal
+            if (witch.getDataWatcher().getWatchableObjectByte(21) == 1) if (witch.ticksExisted % 10 == 0) witch.worldObj.playSoundAtEntity(witch,"soundport:witch.drink",1.0F, pitch);
 
-            if (witch.getDataWatcher().getWatchableObjectByte(21) == 1) { // has to be ==1 because it's a bye not bool
-                if (witch.ticksExisted % 10 == 0) {
-                    witch.worldObj.playSoundAtEntity( // waiter one drinking sound please!
-                        witch,
-                        "soundport:witch.drink", // use the downloaded drinking sound so we can toggle it with the resource pack
-                        1.0F,
-                        pitch
-                    );
-                }
-            }
-        }
-    }
-
-    // throwing mode
-    @SubscribeEvent
-    public void onPotionThrow(EntityJoinWorldEvent event) { // put this whole thing into onEntityPlace
-        if (!event.world.isRemote && event.entity instanceof EntityPotion) {
-            EntityPotion potion = (EntityPotion) event.entity; // potion critter
-
-            if (potion.getThrower() != null && potion.getThrower() instanceof EntityWitch) {
-                event.world.playSoundAtEntity( // waiter one throwing sound please!
-                    potion.getThrower(),
-                    "soundport:witch.throw", // use the downloaded throwing sound so we can toggle it with the resource pack
-                    1.0F,
-                    0.8F + (potion.getThrower().getRNG().nextFloat() * 0.4F) // random pitch 0.8-1.2
-                );
-            }
         }
     }
 
@@ -107,29 +71,21 @@ public class SoundPortEvents {
         Block block = event.placedBlock.getBlock(); // get the block
         Item heldItem = event.player.getHeldItem().getItem(); // get the item if it's a dumb stupid kind of block
 
-        int soundCategory = 0; // category thingy, default to wood
-        String soundArray[] = {
-            "", // 0 BLANK FOR OTHER BLOCKS AND ITEMS
-            "soundport:wood.place", // 1 WOOD
-            "soundport:wool.place", // 2 CLOTH/WOOL
-            "soundport:green.place", // 3 GRASS/CROPS/GREEN
-            "soundport:stone.place" // 4 STONE/IRON
-        };
-
+        int soundCategory = 0;
         float pitch = 0.8F + (event.world.rand.nextFloat() * 0.4F);
         double x = event.pos.getX() + 0.5D;
         double y = event.pos.getY() + 0.5D;
         double z = event.pos.getZ() + 0.5D;
 
-        if ((block instanceof BlockDoor && !(heldItem == Items.iron_door)) || block instanceof BlockBed || block instanceof BlockBanner || event.player.getHeldItem().getItem() == Items.item_frame) {
+        if ((block instanceof BlockDoor && !(heldItem == Items.iron_door)) || block instanceof BlockBed || block instanceof BlockBanner || event.player.getHeldItem().getItem() == Items.item_frame || block instanceof BlockBanner) {
             soundCategory = 1;
         } else if (heldItem instanceof ItemSeeds) {
             soundCategory = 3;
-        } else if (heldItem == Items.iron_door) {
+        } else if (heldItem == Items.iron_door || block instanceof BlockSkull) {
             soundCategory = 4;
         }
 
-        event.world.playSoundEffect( x, y, z, soundArray[soundCategory].toString(),1.0F, pitch);
+        event.world.playSoundEffect(x, y, z, soundArray[soundCategory].toString(),1.0F, pitch);
     }
 
     @SubscribeEvent
@@ -137,14 +93,6 @@ public class SoundPortEvents {
         if (event.world.isRemote) return;
 
         int soundCategory = 0;
-        String soundArray[] = {
-            "", // 0 BLANK FOR OTHER BLOCKS AND ITEMS
-            "soundport:wood.place", // 1 WOOD
-            "soundport:wool.place", // 2 CLOTH/WOOL
-            "soundport:green.place", // 3 GRASS/CROPS/GREEN
-            "soundport:stone.place" // 4 STONE/IRON
-        };
-
         float pitch = 0.8F + (event.world.rand.nextFloat() * 0.4F);
         double x = event.entity.posX + 0.5D;
         double y = event.entity.posY + 0.5D;
@@ -154,6 +102,8 @@ public class SoundPortEvents {
             soundCategory = 2;
         } else if (event.entity instanceof EntityArmorStand) {
             soundCategory = 4;
+        } else if (event.entity instanceof EntityPotion && ((EntityPotion) event.entity).getThrower() instanceof EntityWitch) {
+            soundCategory = 5;
         } else {
             return;
         }
@@ -166,14 +116,6 @@ public class SoundPortEvents {
         if (event.entity.worldObj.isRemote) return;
 
         int soundCategory = 0;
-        String soundArray[] = {
-            "", // 0 BLANK FOR OTHER BLOCKS AND ITEMS
-            "soundport:wood.place", // 1 WOOD
-            "soundport:wool.place", // 2 CLOTH/WOOL
-            "soundport:green.place", // 3 GRASS/CROPS/GREEN
-            "soundport:stone.place" // 4 STONE/IRON
-        };
-
         float pitch = 0.8F + (event.entity.worldObj.rand.nextFloat() * 0.4F);
         double x = event.entity.posX; // DON'T add 0.5D to these since they're entities and aren't on the grid
         double y = event.entity.posY;
